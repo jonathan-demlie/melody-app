6e3134833e44df256665b81a91221bee4069fa40
jonathan-demlie <yonatan.dem123@gmail.com>
2024-02-18 09:55:30 -0500

    fix db MONGODB_URL

--- frontend/src/helpers/axiosInstance.ts
+++ frontend/src/helpers/axiosInstance.ts
@@ -1,8 +1,7 @@
 
 import axios, { AxiosInstance } from 'axios';
 
-const baseURL = 'http://localhost:5000/api';
-// const baseURL = 'mongodb+srv://yonatandem123:ZOgxg3DlPT05uQnA@cluster0.lstmzvp.mongodb.net:5000/api';
+const baseURL = 'https://backend-a0j0.onrender.com:5000/api';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL,
